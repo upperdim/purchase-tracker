75898891902032e62f7ccaf8c1e448bd65b75627
upperdim <45048352+upperdim@users.noreply.github.com>
2022-08-15 21:48:30 +0300

    added item class

---
+++ src/Item.java
@@ -0,0 +1,32 @@
+public class Item {
+    String name;
+    int unitPriceCents;
+    String unitType;
+    int owedUnitQuantity;
+    boolean isPaid;
+    int debtCents;
+
+    public Item(String name, int unitPriceCents, String unitType, int owedUnitQuantity, boolean isPaid, int debtCents) {
+        this.name = name;
+        this.unitPriceCents = unitPriceCents;
+        this.unitType = unitType;
+        this.owedUnitQuantity = owedUnitQuantity;
+        this.isPaid = isPaid;
+        this.debtCents = debtCents;
+    }
+
+    void printItem(int indent) {
+        String prefix = " ".repeat(indent);
+
+        System.out.println("Item name: " + this.name);
+        System.out.println(prefix + "Debt              : " + this.unitPriceCents);
+        System.out.println(prefix + "Paid              : " + this.unitType);
+        System.out.println(prefix + "Owed unit quantity: " + this.owedUnitQuantity);
+        System.out.println(prefix + "Unit price        : " + this.isPaid);
+        System.out.println(prefix + "Unit type         : " + this.debtCents);
+    }
+
+    void printItem() {
+        printItem(0); // default parameter for `indent`
+    }
+}
